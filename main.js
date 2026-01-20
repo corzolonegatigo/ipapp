f2e32baecd68f8833c082a6295a88d195af33711
corzolonegatigo <zlee26369@gmail.com>
2026-01-20 16:17:18 +0800

    3js hello world

---
+++ main.js
@@ -0,0 +1,25 @@
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 55, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 2, 2, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x90ffee } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+
+  cube.rotation.x = 0.5;
+  cube.rotation.y = 0.6;
+
+  renderer.render( scene, camera );
+
+}
